dc1c5d0e15a27990bf223617d3e26b2a2582abde
Ethan Reilly <ethanreilly4@gmail.com>
2023-11-01 13:27:49 +0000

    Fixed Camera

--- Assets/Scripts/Version 1/CameraMove.cs
+++ Assets/Scripts/Version 1/CameraMove.cs
@@ -6,8 +6,16 @@ using UnityEngine.UIElements;
 public class CameraMove : MonoBehaviour
 {
 
+    // [SerializeField]
+    // float camSpeed = 10f;
+
     [SerializeField]
-    float camSpeed = 10f;
+    GameObject target;
+
+    float xOffset = 0;
+    float yOffset = 0;
+    float zOffset = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +26,30 @@ public class CameraMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        /*
         // Need to add a check to see if the camera is at the edge of the map
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(0f, camSpeed * Time.deltaTime, 0f);
         }
 
-        // Need to add a check to see if the camera is at the edge of the map
-        if (Input.GetKey(KeyCode.S))
+        
+        if (Input.GetKey(KeyCode.S) && transform.position.z > -4.13f)
         {
             transform.Translate(0f, -camSpeed * Time.deltaTime, 0f);
         }
+        */
+
+        
+        
+
+    }
+
+    private void LateUpdate()
+    {
+        this.transform.position = new Vector3(xOffset,
+                                              this.transform.position.y,
+                                              target.transform.position.z + zOffset);
     }
 }
