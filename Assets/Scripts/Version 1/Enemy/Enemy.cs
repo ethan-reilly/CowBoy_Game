7bed03b188b649bae6bf7358198213f5eaa2138a
Ethan Reilly <ethanreilly4@gmail.com>
2023-11-16 15:00:47 +0000

    Player shoot function

---
+++ Assets/Scripts/Version 1/Enemy/Enemy.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+// Some code from https://www.youtube.com/watch?v=UjkSFoLxesw
+
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    // Patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    // Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    // States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        // Check if player is in range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patrolling();
+        if (playerInSightRange && !playerInAttackRange) Chasing();
+        if (playerInSightRange && playerInAttackRange) Attacking();
+
+        
+
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        // Check if walkpoint is reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+
+    }
+
+    private void SearchWalkPoint()
+    {
+        // Calculate random point in range
+        float randZ = Random.Range(-walkPointRange, walkPointRange);
+        float randX = Random.Range(-walkPointRange, walkPointRange);
+
+        // Set new random point
+        walkPoint = new Vector3(transform.position.x + randX, transform.position.y, transform.position.z + randZ);
+
+        // Check if point is on the ground
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+
+    private void Chasing()
+    {
+        agent.SetDestination(player.position);
+    }
+    
+    private void Attacking()
+    {
+        // Stop enemy
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+        
+        if(!alreadyAttacked)
+        {
+            /// ATTACK CODE
+            /// Also need to add nav mesh agent 
+
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+
+}
---
+++ Assets/Scripts/Version 1/Projectile/Projectile.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // bullet 
+    public GameObject bullet;
+
+    // bullet speed
+    public float shootForce, upwardForce;
+
+    // gun stats
+    public float timeBetweenShooting, spread, reloadTime, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold = false;
+
+    int bulletsLeft, bulletsShot;
+
+    // bools
+    bool shooting, readyToShoot, reloading;
+
+    // reference
+    public Camera maincamera;
+    public Transform attackPoint;
+
+    // bug fixing
+    public bool allowInvoke = true;
+
+    // graphics
+    //public GameObject muzzleFlash;    //@TODO Muzzle Flash
+    public TextMeshProUGUI ammoDisplay;
+
+    private void Awake()
+    {
+        // make sure magazine is full
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+
+    private void Update()
+    {
+        MyInput();
+
+        if(ammoDisplay != null)
+        {
+            ammoDisplay.SetText("AMMO: " + bulletsLeft / bulletsPerTap + " / " + magazineSize / bulletsPerTap);
+        }
+    }
+
+    private void MyInput()
+    {
+        {
+            // check if allowed to hold down button and take corresponding input
+            if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+            else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+            // Reloading
+            if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading)
+            {
+                Reload();
+            }
+            // Auto Reloading
+            if (readyToShoot && shooting && !reloading && bulletsLeft <= 0)
+            {
+                Reload();
+            }
+
+            // Shooting
+            if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
+            {
+                // Set bullets shot to 0
+                bulletsShot = 0;
+
+                Shoot();
+            }
+        }
+
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        // Finding centre of 2nd camera
+        Ray ray = maincamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        
+        // checking if ray hits something
+        Vector3 targetPoint;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(75);
+        }
+        
+        // calculating the direction from attackpoint to targetpoint
+        Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
+
+        // instansiate bullet
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+        currentBullet.transform.forward = directionWithoutSpread.normalized;
+
+        // adding forces to bullet
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithoutSpread.normalized * shootForce, ForceMode.Impulse);
+        //currentBullet.GetComponent<Rigidbody>().AddForce(maincamera.transform.up * upwardForce, ForceMode.Impulse);
+        
+
+        bulletsLeft--;
+        bulletsShot++;
+
+        
+        if(allowInvoke)
+        {
+            Invoke("ResetShot", timeBetweenShooting);
+            allowInvoke = false;        
+        }
+
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowInvoke = true;
+    
+    }
+
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+    }
+
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
+    
+}
